cd3bd94d73333b3394351d7aae8565df430d0db9
Kelvin Morrison <kelvinninja1@gmail.com>
2021-12-18 21:58:24 +0000

    Update index.php
    *Fixed issues

--- index.php
+++ index.php
@@ -1,40 +1,67 @@
 <?php
 
-include_once 'src/router/Request.php';
-include_once 'src/router/Router.php';
-$router = new Router(new Request);
+    include_once 'src/router/Request.php';
+    include_once 'src/router/Router.php';
+    // Import MCQ class
+    include_once 'src/includes/mcqs/MCQs.php';
 
-$router->get('/', function() {
-    return include 'src/pages/index.php';
-});
+    $router = new Router(new Request);
 
-$router->get('/mcq/primary/1', function($request) {
-    return <<<HTML
-  <h1>Profile</h1>
-HTML;
-});
-$router->get('/mcq/primary/2', function($request) {
-    return <<<HTML
-  <h1>Profile</h1>
-HTML;
-});
-$router->get('/mcq/primary/3', function($request) {
-    return <<<HTML
-  <h1>Profile</h1>
-HTML;
-});
-$router->get('/mcq/primary/4', function($request) {
-    return <<<HTML
-  <h1>Profile</h1>
-HTML;
-});
-$router->get('/mcq/primary/5', function($request) {
-    return <<<HTML
-  <h1>Profile</h1>
-HTML;
-});
-$router->get('/mcq/primary/6', function($request) {
-    return <<<HTML
-  <h1>Profile</h1>
-HTML;
-});
+    $router->get('/', function() {
+        return include 'src/pages/index.php';
+    });
+
+
+    $router->get('/mcq/level/1/qns/10', function($request) {
+        $mcqs = new MCQs(1, 10);
+        return json_encode($mcqs->getMCQs());
+    });
+    $router->get('/mcq/level/1/qns/20', function($request) {
+        $mcqs = new MCQs(1, 20);
+        return json_encode($mcqs->getMCQs());
+    });
+
+    $router->get('/mcq/level/2/qns/10', function($request) {
+        $mcqs = new MCQs(2, 10);
+        return json_encode($mcqs->getMCQs());
+    });
+    $router->get('/mcq/level/2/qns/20', function($request) {
+        $mcqs = new MCQs(2, 20);
+        return json_encode($mcqs->getMCQs());
+    });
+
+    $router->get('/mcq/level/3/qns/10', function($request) {
+        $mcqs = new MCQs(3, 10);
+        return json_encode($mcqs->getMCQs());
+    });
+    $router->get('/mcq/level/3/qns/20', function($request) {
+        $mcqs = new MCQs(3, 20);
+        return json_encode($mcqs->getMCQs());
+    });
+
+    $router->get('/mcq/level/4/qns/10', function($request) {
+        $mcqs = new MCQs(4, 10);
+        return json_encode($mcqs->getMCQs());
+    });
+    $router->get('/mcq/level/4/qns/20', function($request) {
+        $mcqs = new MCQs(4, 20);
+        return json_encode($mcqs->getMCQs());
+    });
+
+    $router->get('/mcq/level/5/qns/10', function($request) {
+        $mcqs = new MCQs(5, 10);
+        return json_encode($mcqs->getMCQs());
+    });
+    $router->get('/mcq/level/5/qns/20', function($request) {
+        $mcqs = new MCQs(5, 20);
+        return json_encode($mcqs->getMCQs());
+    });
+
+    $router->get('/mcq/level/6/qns/10', function($request) {
+        $mcqs = new MCQs(6, 10);
+        return json_encode($mcqs->getMCQs());
+    });
+    $router->get('/mcq/level/6/qns/20', function($request) {
+        $mcqs = new MCQs(6, 20);
+        return json_encode($mcqs->getMCQs());
+    });
