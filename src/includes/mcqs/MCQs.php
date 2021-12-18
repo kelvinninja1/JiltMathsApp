0b4b43ca8dde4ab2dca600b2c70e1db68bcadf4c
Kelvin Morrison <kelvinninja1@gmail.com>
2021-12-18 21:57:41 +0000

    Update MCQs.php

--- src/includes/mcqs/MCQs.php
+++ src/includes/mcqs/MCQs.php
@@ -1,1 +1,39 @@
+<?php
+// import the MCQ class
+require_once('MCQ.php');
+
+//Import the Instruction class
+require_once('Instruction.php');
+
+class MCQs{
+ public $level;
+ public $questions_count;
+ public $questions = [];
+
+    // Create constructor
+    public function __construct($level, $questions_count) {
+        $this->level = $level;
+        $this->questions_count = $questions_count;
+
         // add instructions to questions
+        $this->questions[0] = new Instruction();
+        $this->generateMCQs();
+
+    }
+
+    //generate MCQs
+    public function generateMCQs(){
+        //create an array of MCQs
+
+        for($i = 1; $i <= $this->questions_count; $i++){
+            $mcq = new MCQ($i, $this->level);
+            $this->questions[$i] = $mcq->getMCQ();
+        }
+    }
+
+    //get MCQs
+    public function getMCQs(): array
+    {
+        return $this->questions;
+    }
+}
